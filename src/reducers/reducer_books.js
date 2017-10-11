e2feb8ef704809faa7c916593908af88940664e1
bobrobcpp <king.roob@gmail.com>
2017-10-11 15:31:46 +0100

    Active book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+
+  return [
+  {title: 'Javacript: The Good Parts', pages: 101 },
+  {title: 'Harry Potter', pages: 39},
+  {title: 'The Dark Tower', pages: 10},
+  {title: 'Eloquent Ruby', pages: 1}
+
+  ]
+}
