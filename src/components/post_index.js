c5cef93188dcba5a3a6d3fbf15792994fc753090
bobrobcpp <king.roob@gmail.com>
2017-10-26 12:50:15 +0100

    Basic setup

---
+++ src/components/post_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostIndex extends Component {
+  render() {
+    return (
+      <div>
+      Posts PostIndex
+      </div>
+      );
+  }
+}
+
+export default PostIndex;
